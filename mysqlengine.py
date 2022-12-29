84b8810b70d1ac54080e390640143fbf4dd248bd
jianbindu <121612945+jianbindu@users.noreply.github.com>
2022-12-29 14:04:25 +0200

    Update mysqlengine.py

--- mysqlengine.py
+++ mysqlengine.py
@@ -1,7 +1,10 @@
 import sqlalchemy
 from sqlalchemy import create_engine
 
+#change mysql connection setting.
 engine = sqlalchemy.create_engine('mysql+pymysql://root:root123@localhost:3306/certiq_v2')
+
+#change to you Certiq access information
 params = {
     'username': 'admin@epiroc.com',
     'password': 'admin',
